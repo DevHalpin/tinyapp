28f9a13cff68ed68f444e8795aac2c6bfb4ffed2
DevHalpin <echidnablade@hotmail.com>
2020-09-01 14:33:24 -0700

    Now displays username if username cookie is present

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser());
 const PORT = 8080; //default HTTP port 
 
 app.set('view engine', 'ejs');
@@ -25,7 +27,10 @@ app.get('/', (req,res) => {
 });
 
 app.get('/urls', (req,res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -33,6 +38,11 @@ app.post('/urls', (req,res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL
   res.redirect(`/urls/${shortURL}`)
+})
+
+app.post('/urls/:shortURL/', (req,res) => {
+  urlDatabase[req.params.shortURL] = req.body.newURL
+  res.redirect('/urls')
 })
 
 app.get('/urls/new', (req,res) => {
@@ -45,7 +55,11 @@ app.get('/u/:shortURL', (req,res) => {
 })
 
 app.get('/urls/:shortURL', (req,res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    let templateVars = { 
+      username: req.cookies["username"],
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL]
+    };
     res.render("urls_show", templateVars);
 });
 
@@ -61,6 +75,11 @@ app.get('/urls.json', (req,res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/login", (req,res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
