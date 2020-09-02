6d687ea916d19b0781fedf958e161a76611e29f6
DevHalpin <echidnablade@hotmail.com>
2020-09-01 19:36:14 -0700

    Nicer looking buttons

--- express_server.js
+++ express_server.js
@@ -46,7 +46,10 @@ app.post('/urls/:shortURL/', (req,res) => {
 })
 
 app.get('/urls/new', (req,res) => {
-  res.render('urls_new')
+  let templateVars = { 
+    username: req.cookies["username"],
+  };
+  res.render('urls_new', templateVars)
 })
 
 app.get('/u/:shortURL', (req,res) => {
